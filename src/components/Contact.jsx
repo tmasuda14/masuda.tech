68025052d961687549cc3016d76e3c0d378c6680
Tai Masuda <tmasuda14@gmail.com>
2020-09-12 23:30:29 -0700

    Add contact icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Contact() {
     return (
-        <ul>
-            <li>Email: tmasuda14@gmail.com</li>
-            <li>Text/Cell: 530-518-2745</li>
-            <li>Github: </li>
-            <li>Linkedin: </li>
+        <ul className="contact-list">
+            <li><a className="linkedin-link" href="http://linkedin.com/in/tai-masuda-ba72771b7"><i className="fab fa-linkedin"></i> Linkedin</a></li>
+            <li><a className="github-link" href="http://github.com"><i className="fab fa-github"></i> Github</a></li>
+            <li><i class="far fa-envelope"></i><a className="email-link" href="mailto:tmasuda14@gmail.com"> tmasuda14@gmail.com</a></li>
+            <li><i class="fas fa-mobile-alt"></i> 530-518-2745</li>
         </ul>
     )    
 }
--- src/components/DescriptionCard.jsx
+++ src/components/DescriptionCard.jsx
@@ -9,7 +9,7 @@ function DescriptionCard() {
                         <div className="card-body">
                             <h5 className="card-title">Computer Science Graduate</h5>
                             <p className="card-text">Combining extensive customer service knowledge <br/> with logical, thoughful programming.</p>
-                            <a href="#" className="btn btn-dark">Resume</a>
+                            <a href="http://linkedin.com/in/tai-masuda-ba72771b7" className="btn btn-dark">Resume</a>
                         </div>
                     </div>
                 </div>
@@ -18,7 +18,7 @@ function DescriptionCard() {
                         <div className="card-body">
                             <h5 className="card-title">Continued Learning As A Passion</h5>
                             <p className="card-text">Writing clean, efficient code daily. <br/> Always striving for improvement.  </p>
-                            <a href="#" className="btn btn-dark">Projects</a>
+                            <a href="#projectCarousel" className="btn btn-dark">Projects</a>
                         </div>
                     </div>
                 </div>
