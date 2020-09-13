c36cffabd293325070b5fb98c7ee9c3941ef50b9
Tai Masuda <tmasuda14@gmail.com>
2020-09-12 23:31:59 -0700

    Fix typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Contact() {
     return (
         <ul className="contact-list">
-            <li><a className="linkedin-link" href="http://linkedin.com/in/tai-masuda-ba72771b7"><i className="fab fa-linkedin"></i> Linkedin</a></li>
+            <li><a className="linkedin-link" href="http://linkedin.com/in/tai-masuda-ba72771b7"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
             <li><a className="github-link" href="http://github.com"><i className="fab fa-github"></i> Github</a></li>
             <li><i class="far fa-envelope"></i><a className="email-link" href="mailto:tmasuda14@gmail.com"> tmasuda14@gmail.com</a></li>
             <li><i class="fas fa-mobile-alt"></i> 530-518-2745</li>
