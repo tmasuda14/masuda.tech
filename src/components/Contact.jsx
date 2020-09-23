eae7b670bda48759dc1369460b7b671d605efc5d
Tai Masuda <tmasuda14@gmail.com>
2020-09-22 19:51:02 -0700

    Update CSS for navbar contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,9 @@ function Contact() {
     return (
         <ul className="contact-list">
             <li><a className="linkedin-link" href="http://linkedin.com/in/tai-masuda-ba72771b7"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
-            <li><a className="github-link" href="http://github.com"><i className="fab fa-github"></i> Github</a></li>
-            <li><i class="far fa-envelope"></i><a className="email-link" href="mailto:tmasuda14@gmail.com"> tmasuda14@gmail.com</a></li>
-            <li><i class="fas fa-mobile-alt"></i> 530-518-2745</li>
+            <li><a className="github-link" href="https://github.com/tmasuda14"><i className="fab fa-github"></i> Github</a></li>
+            <li><i class="far fa-envelope"></i><a className="email-cell-link" href="mailto:tmasuda14@gmail.com"> tmasuda14@gmail.com</a></li>
+            <li><i class="fas fa-mobile-alt"></i><a className="email-cell-link" href="tel:15305182745"> 530-518-2745</a></li>
         </ul>
     )    
 }
